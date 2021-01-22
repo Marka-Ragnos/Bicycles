88d8b78a49d5a25d4d647dc4e5ae13d051a68c7a
Саня <dunkan-1@yandex.ru>
2021-01-22 14:52:20 +0300

    баг фикс

--- source/js/main.js
+++ source/js/main.js
@@ -3,16 +3,31 @@
 (function () {
   var headerNav = document.querySelector(".header__nav");
   var headerToggle = document.querySelector(".nav__toggle");
+  var bodyNode = document.body;
+  var navLinks = document.querySelectorAll(".nav__item");
 
   headerNav.classList.remove("nav--nojs");
+
+    if (navLinks) {
+      Array.prototype.forEach.call(navLinks, function (link) {
+        link.addEventListener("click", function () {
+          // console.log(navLinks);
+          if (bodyNode.classList.contains("scroll-lock")) {
+            bodyNode.classList.remove("scroll-lock");
+          }
+        });
+      });
+    }
 
   headerToggle.addEventListener("click", function () {
     if (headerNav.classList.contains("nav--closed")) {
       headerNav.classList.remove("nav--closed");
       headerNav.classList.add("nav--opened");
+      bodyNode.classList.add("scroll-lock");
     } else {
       headerNav.classList.add("nav--closed");
       headerNav.classList.remove("nav--opened");
+      bodyNode.classList.remove("scroll-lock");
     }
   });
 })();
