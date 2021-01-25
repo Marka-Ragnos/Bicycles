c7c5c1d0ca6558341637c9b72fabba31ecb38e37
Саня <dunkan-1@yandex.ru>
2021-01-25 13:47:54 +0300

    bugs fix

--- source/js/main.js
+++ source/js/main.js
@@ -8,16 +8,15 @@
 
   headerNav.classList.remove("nav--nojs");
 
-    if (navLinks) {
-      Array.prototype.forEach.call(navLinks, function (link) {
-        link.addEventListener("click", function () {
-          // console.log(navLinks);
-          if (bodyNode.classList.contains("scroll-lock")) {
-            bodyNode.classList.remove("scroll-lock");
-          }
-        });
+  if (navLinks) {
+    Array.prototype.forEach.call(navLinks, function (link) {
+      link.addEventListener("click", function () {
+        if (bodyNode.classList.contains("scroll-lock")) {
+          bodyNode.classList.remove("scroll-lock");
+        }
       });
-    }
+    });
+  }
 
   headerToggle.addEventListener("click", function () {
     if (headerNav.classList.contains("nav--closed")) {
